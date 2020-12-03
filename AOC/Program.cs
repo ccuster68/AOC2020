7782ba974b1ef645b354476f84721d5428f4bc86
ccuster68 <charles@custerfamily.com>
2020-12-02 16:43:01 -0800

    Correct 2-1

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -11,25 +11,26 @@ namespace AOC
     {
         static void Main(string[] args)
         {
-            var inputFile = @"e:\git\aoc2020\input\Day1A.txt";
-            var input = File.ReadAllLines(inputFile).Select(y => int.Parse(y)).ToArray();
-            var minNum = input.Min();
-            var maxNum = input.Max();
+            var inputFile = @"e:\git\aoc2020\input\Day2A.txt";
+            var input = File.ReadAllLines(inputFile);//.Select(y => int.Parse(y)).ToArray();
 
-            for (int i = 0; i < input.Length - 2; i++)
+            var goodPasswords = 0;
+            foreach (var i in input)
             {
-                for (int j = i + 1; j < input.Length - 1; j++)
-                {
-                    if (input[i] + input[j] + minNum > 2020)
-                        continue;
-                    for (int k = j + 1; k < input.Length; k++)
-                    {
-                        if (input[i] + input[j] + input[k] == 2020)
-                            Console.WriteLine(input[i] * input[j] * input[k]);
-                    }
-                }
+                var lines = i.Split(' ');
+                var least = lines[0].Split('-').Select(y=>int.Parse(y)).ToArray()[0];
+                var most = lines[0].Split('-').Select(y => int.Parse(y)).ToArray()[1];
+
+                var passwordChar = lines[1][0];
+
+                var charCount = lines[2].Count(y => y == passwordChar);
+
+
+                if (charCount >= least && charCount <= most) goodPasswords++;
             }
 
+            Console.WriteLine(goodPasswords);
+            
         }
     }
 }
