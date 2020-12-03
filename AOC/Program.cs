d707681fc0e07cf300f3f56aa8ace17dd28eefc5
ccuster68 <charles@custerfamily.com>
2020-12-03 10:22:30 -0800

    Day3-2 answer

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -13,17 +13,26 @@ namespace AOC
         {
             var inputFile = @"e:\git\aoc2020\input\Day3A.txt";
             var lines = File.ReadAllLines(inputFile);//.Select(y => int.Parse(y)).ToArray();
+            List<(int, int)> slopes = new List<(int, int)>();
+            slopes.Add((1, 1));
+            slopes.Add((3, 1));
+            slopes.Add((5, 1));
+            slopes.Add((7, 1));
+            slopes.Add((1, 2));
 
-            var trees = 0;
-            for (int i = 1; i < lines.Length; i++)
+            Int64 ans = 1;
+
+            foreach (var slope in slopes)
             {
-                // make sure we have enough repeats
-                var line = string.Concat((Enumerable.Repeat(lines[i],3 * (i))));
-
-                if (line[3 * i] == '#') trees++;
-                
+                var trees = 0;
+                for (int i = slope.Item2; i < lines.Length; i +=slope.Item2)
+                {
+                    // make sure we have enough repeats
+                    var line = string.Concat((Enumerable.Repeat(lines[i], (slope.Item1+1) * (i))));
+                    if (line[slope.Item1 * i/slope.Item2] == '#') trees++;
+                }
             }
-            Console.WriteLine(trees);
+            Console.WriteLine(ans);
         }
     }
 }
