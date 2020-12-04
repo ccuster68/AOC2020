3da8c601c5c226fb92476af74b0021cdb1447f2e
ccuster68 <charles@custerfamily.com>
2020-12-04 10:32:24 -0800

    answer

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AOC
 {
@@ -18,13 +19,14 @@ namespace AOC
                 // separate each passport into it's parts
                 var parts = passport.Replace($"{Environment.NewLine}", "~").Replace(' ', '~').Split('~');
                 if (parts.Length < 7) continue;
-                if (!parts.Where(p => p.Substring(0, 4) == "byr:").Any()) continue;
-                if (!parts.Where(p => p.Substring(0, 4) == "iyr:").Any()) continue;
-                if (!parts.Where(p => p.Substring(0, 4) == "eyr:").Any()) continue;
-                if (!parts.Where(p => p.Substring(0, 4) == "hgt:").Any()) continue;
-                if (!parts.Where(p => p.Substring(0, 4) == "hcl:").Any()) continue;
-                if (!parts.Where(p => p.Substring(0, 4) == "ecl:").Any()) continue;
-                if (!parts.Where(p => p.Substring(0, 4) == "pid:").Any()) continue;
+                if (!parts.Where(p => Regex.Match(p, "^byr:(19[2-9]\\d|200[0-2])$").Success).Any()) continue;
+                if (!parts.Where(p => Regex.Match(p, "^iyr:(201\\d|2020)$").Success).Any()) continue;
+                if (!parts.Where(p => Regex.Match(p, "^eyr:(202\\d|2030)$").Success).Any()) continue;
+                if (!parts.Where(p => Regex.Match(p, "^hgt:(1[5-8]\\dcm|19[0-3]cm|59in|6\\din|7[0-6]in)$").Success).Any()) continue;
+                if (!parts.Where(p => Regex.Match(p, "^hcl:(#[a-f0-9]{6})$").Success).Any()) continue;
+                if (!parts.Where(p => Regex.Match(p, "^ecl:(amb|blu|brn|gry|grn|hzl|oth)$").Success).Any()) continue;
+                if (!parts.Where(p => Regex.Match(p, "^pid:\\d{9}$").Success).Any()) continue;
+
                 ans++;
             }
             Console.WriteLine(ans);
