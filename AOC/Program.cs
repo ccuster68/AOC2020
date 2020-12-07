5b7e170187ff327affdcadbc203d6f85a62dd065
ccuster68 <charles@custerfamily.com>
2020-12-07 12:58:25 -0800

    still working

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -8,25 +8,45 @@ namespace AOC
 {
     class Program
     {
+        static string[] lines;
         static void Main(string[] args)
         {
-            var inputFile = @"e:\git\aoc2020\input\Day7A.txt";
-            var lines = File.ReadAllLines(inputFile);
-            // Get direct rules that contain shiny gold bags
-            
-            var bagList = lines.Where(r => Regex.Match(r, "contain .*shiny gold bag").Success).Select(r => r.Substring(0, r.IndexOf("s contain "))).ToList();
-            do
-            {
-                var lastCount = bagList.Count;
-                // Add bags that are not in the bagList and can contain a bag that contains the shiny gold bag
-                bagList.AddRange(
-                    lines.Where(r => !bagList.Contains(r.Substring(0, r.IndexOf("s contain "))) && bagList.Any(bl => Regex.Match(r, $"contain.* {bl}").Success))
-                    .Select(r => r.Substring(0, r.IndexOf("s contain "))).ToList());
-                if (bagList.Count==lastCount) break;
-            } while (true) ;
+            var inputFile = @"e:\git\aoc2020\input\Day7ATEST.txt";
+            lines = File.ReadAllLines(inputFile);
+            // get bags that are in shiny gold bag
 
-                        
-            Console.WriteLine(bagList.Count);
+            Int64 count = GetBagCount("1 shiny gold bag");
+
+            Console.WriteLine(count);
+
+        }
+
+        private static Int64 GetBagCount(string bagInfo)
+        {
+            int multiplier;
+            // when first string is not a number, then there are no more container bags
+            try
+            {
+                multiplier = int.Parse(bagInfo.Split(' ').First());
+            }
+            catch
+            {
+                return 1;
+            }
+            Int64 retValue = multiplier;
+            var bagName = bagInfo.Replace(".","").Substring(multiplier.ToString().Length + 1) + (multiplier > 1 ? " contain " : "s contain ");
+
+            // Get list of bags in this bag
+            var bagList = lines.Where(r => Regex.Match(r, $"^{bagName}").Success)
+                                .Select(r => r.Substring(bagName.Length)).First().Split(',').Select(bl => bl.Trim()).ToList();
+
+
+            // assuming bags always have container
+            foreach (var bag in bagList)
+            {
+                retValue += GetBagCount(bag);
+            }
+            return retValue;
         }
     }
 }
