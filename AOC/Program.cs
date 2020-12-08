50dac0ab0d6a71b0dec6884f2852facc2a505eb5
ccuster68 <charles@custerfamily.com>
2020-12-08 14:49:31 -0800

    Initial work.

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -8,24 +8,60 @@ namespace AOC
 {
     class Program
     {
-
+        static int acc = 0;
         static void Main(string[] args)
         {
-            var inputFile = @"e:\git\aoc2020\input\Day8A.txt";
-            var lines = File.ReadAllLines(inputFile);
+            var inputFile = @"e:\git\aoc2020\input\Day8ATEST.txt";
+            var input = File.ReadAllText(inputFile);
+            var index = 0;
+            var acc = 0;
+            do
+            {
+                var changedInput = "";
+                index = input.IndexOf("jmp", index);
+                if (index > 0)
+                {
+                    changedInput = input.Substring(0, index) + "nop" + input.Substring(index + 3);
+                    if (process(changedInput))
+                    {
+                        Console.WriteLine(acc);
+                        break;
+                    }
+
+                }
+                else
+                {
+                    index = 0;
+                    changedInput = input.Substring(0, index) + "nop" + input.Substring(index + 3);
+                }
+
+
+                if (process(changedInput))
+                {
+                    Console.WriteLine(acc);
+                    break;
+                }
+
+            } while (true);
+        }
+
+        static bool process(string input)
+        {
+            acc = 0;
+            var lines = input.Split('\n');
+
             var operations = new List<(string op, int arg)>();
             var operationsHit = new List<int>();
-            var acc = 0;
+
             var pos = 0;
             foreach (var line in lines)
             {
                 operations.Add((line.Split(' ')[0], int.Parse(line.Split(' ')[1])));
             }
 
-
-
             do
             {
+                if (pos == operations.Count) return true;
                 if (operationsHit.Contains(pos)) break;
 
                 operationsHit.Add(pos);
@@ -47,7 +83,8 @@ namespace AOC
                 }
             } while (true);
 
-            Console.WriteLine(acc);
+            return false;    
         }
+
     }
 }
