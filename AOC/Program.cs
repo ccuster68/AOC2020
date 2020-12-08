f5d2c1c1e76c7c08c6110116c14f945946996734
ccuster68 <charles@custerfamily.com>
2020-12-08 13:28:08 -0800

    correct answer

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -9,37 +9,30 @@ namespace AOC
     class Program
     {
         static string[] lines;
-        static List<(int numberOfBags, int multiple)> listOfBags = new List<(int, int)>();
+        static List<int> listOfBags = new List<int>();
         
         static void Main(string[] args)
         {
-            var inputFile = @"e:\git\aoc2020\input\Day7ATEST.txt";
+            var inputFile = @"e:\git\aoc2020\input\Day7A.txt";
             lines = File.ReadAllLines(inputFile);
             // get bags that are in shiny gold bag
 
             ProcessBag("1 shiny gold bag", 1);
 
-            Double count = 0;
-            foreach (var bag in listOfBags)
-            {
-                count += bag.multiple;
-            }
+            //Double count = 0;
+            //foreach (var bagCount in listOfBags)
+            //{
+            //    count += bag.multiple;
+            //}
 
-            Console.WriteLine(count);
+            Console.WriteLine(listOfBags.Sum());
         }
 
         // Dictionary of level / bag
         private static void ProcessBag(string bagInfo, int multiple)
         {
-            var numberOfBags = 0;
-            try
-            {
-                numberOfBags = int.Parse(bagInfo.Split(' ').First());
-            }
-            catch
-            {
-                return;
-            }
+            var numberOfBags = int.Parse(bagInfo.Split(' ').First());
+
             // Will turn 1 shiny gold bags contain 2 dark red bags. to shiny gold bags contain
             var bagName = bagInfo.Replace(".", "").Substring(numberOfBags.ToString().Length + 1);
             bagName += bagName.EndsWith("bags") ? "" : "s";
@@ -48,13 +41,13 @@ namespace AOC
             var bagList = lines.Where(r => Regex.Match(r, $"^{bagSearchName}").Success)
                                 .Select(r => r.Substring(bagSearchName.Length)).First().Split(',').Select(bl => bl.Trim()).ToList();
 
-            List<(string bag, int level)> bagsToProcess = new List<(string bag, int level)>();
+            List<(string bag, int multiple)> bagsToProcess = new List<(string, int)>();
             foreach (var bag in bagList)
             {
                 try
                 {
-                    numberOfBags = int.Parse(bag.TrimStart().Split(' ').First());
-                    listOfBags.Add((numberOfBags, numberOfBags*multiple));
+                    numberOfBags = int.Parse(bag.TrimStart().Split(' ').First())*multiple;
+                    listOfBags.Add(numberOfBags);
                     bagsToProcess.Add((bag, numberOfBags));
                 }
                 catch
@@ -64,7 +57,7 @@ namespace AOC
             }
             foreach (var bag in bagsToProcess)
             {
-                ProcessBag(bag.bag, bag.level);
+                ProcessBag(bag.bag, bag.multiple);
             }
 
         }
