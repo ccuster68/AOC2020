9903ce4eda3d3bb2040f46987363d6a58281de91
ccuster68 <charles@custerfamily.com>
2020-12-10 03:40:54 -0800

    answer

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -11,69 +11,23 @@ namespace AOC
         static int acc;
         static void Main(string[] args)
         {
-            var inputFile = @"e:\git\aoc2020\input\Day9A.txt";
+            var inputFile = @"e:\git\aoc2020\input\Day10.txt";
             // get and put input into array of operation, arg
-            var input = File.ReadAllLines(inputFile).Select(i => Int64.Parse(i)).ToArray();
-            const int preamble = 25;
-            var match = false;
-            var nonMatchinNumber = 0l;
-            var indexNotFound = 0;
-            // get non matching number                       
-            for (int i = preamble; i < input.Length; i++)
+            var input = File.ReadAllLines(inputFile).Select(i => Int64.Parse(i)).ToList();
+            input.Add(0);
+            input.Sort();
+            var oneJolt = 0;
+            var threeJolt = 1;
+
+            for (int i = input.Count-1; i >= 1; i--)
             {
-                for (int j = i - 1; j > i - preamble; j--)
-                {
-                    for (int k = j - 1; k >= i - preamble; k--)
-                    {
-                        // is there a match
-                        if (input[j] + input[k] == input[i])
-                        {
-                            match = true;
-                            break;
-                        }
-                    }
-                    if (match)
-                        break;
-                }
-                if (!match)
-                {
-                    nonMatchinNumber = input[i];
-                    indexNotFound = i;
-                    break;
-                }
-                else
-                {
-                    match = false;
-                }
+                var diff = input[i] - input[i - 1];
+                if (diff == 3) threeJolt++;
+                if (diff == 1) oneJolt++;
+
+                
             }
-
-            
-            // now find congiguous match of non match
-            for (int i = 0; i < indexNotFound; i++)
-            {
-                long value = input[i];
-                var k = i;
-                do
-                {
-                    k++;
-                    if (k == input.Length) break;
-                    value += input[k];
-                } while (value < nonMatchinNumber);
-                if (value == nonMatchinNumber)
-                {
-                    var max = 0l;
-                    var min = long.MaxValue;
-                    for (int j = i; j <= k; j++)
-                    {
-                        max = Math.Max(max, input[j]);
-                        min = Math.Min(min, input[j]);
-                    }
-                    Console.WriteLine(max+min);
-                    break;
-                }             
-            }
-
-
+            Console.WriteLine(oneJolt*threeJolt);
         }
     }
 }
