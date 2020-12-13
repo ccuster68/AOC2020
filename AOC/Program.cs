34a4d0b92df14806e47f5ab98cc75066c5ca9562
ccuster68 <charles@custerfamily.com>
2020-12-13 12:29:54 -0800

    buses correct

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -11,22 +11,46 @@ namespace AOC
     {
         static void Main(string[] args)
         {
-            var inputFile = @"e:\git\aoc2020\input\Day13.txt";
+            var inputFile = @"e:\git\aoc2020\input\Day13test.txt";
             var inputs = File.ReadAllLines(inputFile);
             var earliestTime = int.Parse(inputs[0]);
-            var busIds = inputs[1].Split(',').Where(b => int.TryParse(b, out var a)).Select(b => int.Parse(b)).ToList();
+            var busIds = inputs[1].Split(',');
 
-            List<(int earliestTime, int busId)> lFound = new List<(int, int)>();
-            var bestTime = int.MaxValue;
-            var bestBusId = 0;
-            foreach (var busId in busIds)
+            var firstBus = int.Parse(busIds[0]);
+            var XsInBetween = 0;
+            List<(int busId, int x)> buses = new List<(int busId, int tsDiff)>() { (firstBus, 0) };
+
+            var inc = 0;
+            for (int i = 1; i < busIds.Length; i++)
             {
-                var busTime = ((earliestTime / busId) + 1) * busId;
-                var curBestTime = bestTime;
-                bestTime = Math.Min(bestTime, busTime);
-                if (curBestTime > bestTime) bestBusId = busId;
+                
+                if (busIds[i] == "x") XsInBetween++;
+                else
+                {
+                    inc++;
+                    buses.Add((int.Parse(busIds[i]), XsInBetween+inc));
+                }
             }
-            Console.WriteLine((bestTime - earliestTime) * bestBusId);
+            var found = false;
+            long firstBusTS = 0;
+            do
+            {
+                firstBusTS += firstBus;
+
+                long newTS = firstBusTS;
+                for (int i = 1; i < buses.Count; i++)
+                {
+                    newTS += buses[i].x + 1;
+                    if (newTS % buses[i].busId != 0)
+                    {
+                        break;
+                    }
+                    // if we make it here and we are on last buss we found it
+                    if (i == buses.Count - 1) found = true;
+                }
+            } while (!found);
+            Console.WriteLine(firstBusTS);
+            Console.ReadLine();
         }
     }
 }
