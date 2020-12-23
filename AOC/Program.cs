b08f913b823b233fdd55b31625f2404fa9f8e4c9
ccuster68 <charles@custerfamily.com>
2020-12-22 16:54:06 -0800

    getting there

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -11,36 +11,57 @@ namespace AOC
     {
         static void Main(string[] args)
         {
-            var inputFile = @"e:\git\aoc2020\input\Day14.txt";
+            var inputFile = @"e:\git\aoc2020\input\Day14test.txt";
             var inputs = File.ReadAllLines(inputFile);
 
             long ans = 0;
             var mask = new char[36];
             var memories = new Dictionary<int, long>();
-            for (long i = 0; i < inputs.Length; i++)
+            for (int i = 0; i < inputs.Length; i++)
             {
                 var ansArray = new char[36];
                 var memToAdd = inputs[i].Replace("mem[", "").Replace("] =", "").Split(' ');
 
                 //List<(long location, long value)> memories = new List<(long, long)>();
                 if (inputs[i].StartsWith("mask"))
-                { 
-                    mask = inputs[i].Split(' ')[2].ToCharArray();  
+                {
+                    mask = inputs[i].Split(' ')[2].ToCharArray();
                 }
                 else
                 {
+                    // calculate locations to add value to
+                    var value = long.Parse(memToAdd[1]);
+                    var locations = new string[int.Parse(Math.Pow(2, mask.Count(m => m == 'X')).ToString())];
+
                     // calculate value to add
                     (int location, long value) mem = (int.Parse(memToAdd[0]), long.Parse(memToAdd[1]));
                     // get masked value
-                    var binary = Convert.ToString(mem.value, 2).PadLeft(36, '0');
+                    var binary = Convert.ToString(mem.location, 2).PadLeft(36, '0');
+                    var toAdd = "";
                     for (int j = 0; j < 36; j++)
                     {
-                        ansArray[j] = ((binary[j] == '1' && mask[j] != '0') || mask[j] == '1') ? '1' : '0';
+                        if (mask[j] == 'X')
+                        {
+                            for (int k = 0; k < locations.Length; k++)
+                            {
+                                locations[k] += toAdd + (k % 2 == 0 ? "0" : "1");
+                                if (j == 35)
+                                {
+                                    var loc = Convert.ToInt32(locations[i]);
+                                    if (memories.ContainsKey(loc))
+                                        memories[loc] = value;
+                                    else
+                                        memories.Add(loc, value);
+                                }
+                            }
+                            toAdd = "";
+                        }
+                        else
+                            toAdd += binary[j];
                     }
-                    memories[mem.location]= Convert.ToInt64(new String(ansArray), 2);
                 }
 
-                if (i==inputs.Length-1) ans += memories.Select(m => m.Value).Sum();
+                if (i == inputs.Length - 1) ans += memories.Select(m => m.Value).Sum();
             }
 
 
